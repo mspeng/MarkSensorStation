44b0ea01c221c39272e818d6fd148a1db2b3bb4d
Mark Peng <mspeng@gmail.com>
2024-12-01 22:01:17 -0800

    Adjusted light sensitivity again

--- repeatgetsensortosqldb.py
+++ repeatgetsensortosqldb.py
@@ -55,7 +55,7 @@ numpersec = 10
 fastsampTD = dt.timedelta(seconds=1/numpersec)
 slowsampTD = dt.timedelta(seconds=10/numpersec)
 TIMEWINDOWTD = dt.timedelta(seconds=60)    # seconds
-varThresDi = {'ldr':200,
+varThresDi = {'ldr':400,
               'acc':49,
               'gyr':4,
               'mag':25,
